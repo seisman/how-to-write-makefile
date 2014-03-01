c8c12f6d68b0165d4990e834f538de73bf8af2f2
Dongdong Tian <seisman.info@gmail.com>
2014-03-01 19:40:06 +0800

    PDF配置完毕

--- source/conf.py
+++ source/conf.py
@@ -185,7 +185,7 @@ htmlhelp_basename = 'Makefiledoc'
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
 #'pointsize': '10pt',
@@ -206,8 +206,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'Makefile.tex', u'跟我一起写Makefile Documentation',
-   u'陈皓', 'manual'),
+  ('index', 'Makefile.tex', u'跟我一起写Makefile',
+   u'陈皓', 'manual', True),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -236,7 +236,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'makefile', u'跟我一起写Makefile Documentation',
+    ('index', 'makefile', u'跟我一起写Makefile',
      [u'陈皓'], 1)
 ]
 
@@ -250,7 +250,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'Makefile', u'跟我一起写Makefile Documentation',
+  ('index', 'Makefile', u'跟我一起写Makefile',
    u'陈皓', 'Makefile', 'One line description of project.',
    'Miscellaneous'),
 ]
