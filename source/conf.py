13b125042ed3e875229edaf0981bc34c60b51532
Dongdong Tian <seisman.info@gmail.com>
2014-03-07 10:17:23 +0800

    11pt

--- source/conf.py
+++ source/conf.py
@@ -187,7 +187,7 @@ latex_elements = {
 'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+'pointsize': '11pt',
 
 # Additional stuff for the LaTeX preamble.
 'preamble': r'''
