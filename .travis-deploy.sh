4abde6d82e4de191b7c893554ca5486e2910791d
Dongdong Tian <seisman.info@gmail.com>
2016-05-31 13:17:10 +0800

    silent push

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -10,10 +10,10 @@ git config user.email "${mail}"
 
 # Deploy Github Pages
 ghp-import -b gh-pages -n build/html -m 'Update by travis automatically'
-git push "https://${GH_TOKEN}@${GH_REF}" gh-pages:gh-pages --force
+git push "https://${GH_TOKEN}@${GH_REF}" gh-pages:gh-pages --force --quiet
 
 # Deploy offline HTML and PDF files
 mkdir build/${docdir} && cd build
 cp latex/${pdfname} ${docdir}/
 ghp-import -b ${docdir} ${docdir} -m 'Update by travis automatically'
-git push "https://${GH_TOKEN}@${GH_REF}" ${docdir}:${docdir} --force
+git push "https://${GH_TOKEN}@${GH_REF}" ${docdir}:${docdir} --force --quiet
